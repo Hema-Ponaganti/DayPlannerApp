cf01dfaf72feb50d06f8b794439b48d3e03e0718
Hema-Ponaganti <ponagaha@mail.uc.edu>
2024-04-21 18:06:55 -0400

    add dayPlanScreen navigation

--- screens/DayPlanScreen.js
+++ screens/DayPlanScreen.js
@@ -1,12 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
-const DayPlanScreen = () => {
+const DayPlanScreen = ({ route }) => {
+  let { tasks } = route.params;
+
+  tasks = tasks.sort((a, b) => {
+    // Assuming each task has a `time` property representing the task time
+    return new Date(a.time) - new Date(b.time);
+  });
+  
+
   return (
-    <View>
-      <Text>Day Plan Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Tasks for selected date:</Text>
+      {tasks.map((task, index) => (
+        <View key={index} style={styles.taskContainer}>
+          <View style={styles.taskBox}>
+            <Text style={styles.taskName}>Task Name: {task.taskName}</Text>
+            <Text style={styles.time}>Time: {new Date(task.time).toLocaleTimeString()}</Text>
+            <Text style={styles.priority}>Priority: {task.priority}</Text>
+          </View>
+        </View>
+      ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  taskContainer: {
+    marginBottom: 20,
+  },
+  taskBox: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+  },
+  taskName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  time: {
+    fontSize: 14,
+  },
+  priority: {
+    fontSize: 14,
+    fontStyle: 'italic',
+  },
+});
 
 export default DayPlanScreen;
