c64f341fc11f960ca9f4ef33d558cf7d8e332ec7
Hema-Ponaganti <ponagaha@mail.uc.edu>
2024-04-21 15:20:09 -0400

    Use radio button instead of text field for priority

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -1,12 +1,11 @@
-// TaskForm.js
-
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { RadioButton } from 'react-native-paper';
 
 const TaskForm = ({ selectedDate, onSave, onClose }) => {
   const [taskName, setTaskName] = useState('');
   const [time, setTime] = useState('');
-  const [priority, setPriority] = useState('');
+  const [priority, setPriority] = useState(''); // State to hold selected priority
 
   const handleSave = () => {
     onSave({ taskName, time, priority });
@@ -31,12 +30,34 @@ const TaskForm = ({ selectedDate, onSave, onClose }) => {
         value={time}
         onChangeText={setTime}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Priority"
-        value={priority}
-        onChangeText={setPriority}
-      />
+      <Text style={styles.subHeading}>Priority</Text>
+      <View style={styles.radioContainer}>
+        <RadioButton.Group onValueChange={(value) => setPriority(value)} value={priority}>
+          <View style={styles.radioButton}>
+            <RadioButton.Item
+              label="High"
+              value="High"
+              status={priority === 'High' ? 'checked' : 'unchecked'}
+              onPress={() => setPriority('High')}
+              color="#007AFF"
+            />
+            <RadioButton.Item
+              label="Medium"
+              value="Medium"
+              status={priority === 'Medium' ? 'checked' : 'unchecked'}
+              onPress={() => setPriority('Medium')}
+              color="#007AFF"
+            />
+            <RadioButton.Item
+              label="Low"
+              value="Low"
+              status={priority === 'Low' ? 'checked' : 'unchecked'}
+              onPress={() => setPriority('Low')}
+              color="#007AFF"
+            />
+          </View>
+        </RadioButton.Group>
+      </View>
       <View style={styles.buttonContainer}>
         <Button title="Save" onPress={handleSave} color="blue" />
         <Button title="Cancel" onPress={onClose} color="red" />
@@ -58,12 +79,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 10,
   },
+  subHeading: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
   input: {
     borderWidth: 1,
     borderColor: 'gray',
     padding: 10,
     marginBottom: 10,
     borderRadius: 5,
+  },
+  radioContainer: {
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  radioButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   buttonContainer: {
     flexDirection: 'row',
